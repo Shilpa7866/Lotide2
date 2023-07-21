101c71c18ee35b7c25e8ed855cc9a72c59fc6808
Shilpa <shilpa.pulipati24@gmail.com>
2023-07-20 22:55:13 -0400

    adding letterPositions.js file

---
+++ letterPositions.js
@@ -0,0 +1,40 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+   return true;
+
+};
+
+const assertArraysEqual = function(actual, expected) {
+  let arr1 = [];
+  let arr2 = [];
+  actual = eqArrays(arr1, arr2);
+  
+  if (actual === eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== eqArrays(arr1, arr2)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+    results[sentence[i]].push(i);
+  } else {
+    results[sentence[i]] = [i];
+  }
+    
+}
+   return results;
+};
+
+console.log(letterPositions("hello"));
+console.log(letterPositions("lighthouse in the house"));
