02c5d7ab57ff9cf244f77e08a5d6776662c529be
Shilpa <shilpa.pulipati24@gmail.com>
2023-08-04 00:11:35 -0400

    refactoring middle.js file

--- middle.js
+++ middle.js
@@ -1,26 +1,7 @@
-const assertArraysEqual = function(actual, expected) {
-  let arr1 = [];
-  let arr2 = [];
-  actual = eqArrays(arr1, arr2);
-  
-  if (actual === eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== eqArrays(arr1, arr2)) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual'); 
 
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+
 
 const middle = function(numArray) {
   var middleVal = Math.floor(numArray.length / 2);
@@ -31,6 +12,6 @@ const middle = function(numArray) {
   }
 };
 
-console.log(middle([1,3,5])); // expected => []
-console.log(middle([1,2,3])); // expected => [2]
-console.log(middle([1,2,3,4,5,6])); // expected => [3]
+
+
+module.exports = middle;
