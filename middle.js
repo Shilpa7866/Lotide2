9df63e68b334c79e6f15d89930837df5999b9d5e
Shilpa <shilpa.pulipati24@gmail.com>
2023-08-04 11:35:49 -0400

    refactoring middle.js file

--- middle.js
+++ middle.js
@@ -1,17 +1,23 @@
 const eqArrays = require('./eqArrays');
-const assertArraysEqual = require('./assertArraysEqual'); 
+const assertArraysEqual = require('./assertArraysEqual');
 
-
-
-const middle = function(numArray) {
-  var middleVal = Math.floor(numArray.length / 2);
-  if(numArray.length % 2 === 0) {
-    return (numArray[middleVal - 1]  + "," + numArray[middleVal]);
-  } else {
-    return (numArray[middleVal]);
+const middle = function (array) {
+  if (array.length <= 2) {
+    return []
   }
-};
+  if (array.length % 2 === 0) {
+    const index2 = array.length / 2
+    const index1 = index2 - 1
+    const value1 = array[index1]
+    const value2 = array[index2]
+    return [value1, value2]
+  }
+  const index = (array.length - 1) / 2
+  const value = array[index]
+  return [value]
+}
 
+module.exports = middle;
 
 
 module.exports = middle;
