44f8293dbc25d6550a81e0df73d5c65f398aac18
Shilpa <shilpa.pulipati24@gmail.com>
2023-08-04 14:24:30 -0400

    adding test cases for mocha and chai testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,20 @@
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');  // imports tail function
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+describe('#tail', () => {
+  it("returns a new array containing all elements of the array except for the first one", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it("should not modify the original array when called an array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    //tail(words);
+    assert.deepEqual(tail(words), (["Lighthouse", "Labs"]));
+  });
+});
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
 
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+
+
 
 module.exports = tail;
