042dff9e91deceb205a2564a9ae7abc4d0c29f08
Shilpa <shilpa.pulipati24@gmail.com>
2023-08-04 00:03:35 -0400

    refactoring assertArraysEqual.js file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,5 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-   return true;
 
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   let arr1 = [];
@@ -23,5 +13,6 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertArraysEqual(eqArrays([1, 2, 3, 4], [3, 2, 1]), false);
+
+
+module.exports = assertArraysEqual;
