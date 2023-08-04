0673dbc867a828da9070a0bbd0f41b4355ac6261
Shilpa <shilpa.pulipati24@gmail.com>
2023-08-04 11:29:07 -0400

    adding test cases

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,23 @@
+const assert = require('chai').assert;
 const middle = require ('../middle');
 
+ describe("#middle", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
 
-console.log(middle([1,3,5])); // expected => []
+  it("returns 3 for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+ 
+   it("return [] for [1,2]", () => {
+    assert.deepEqual(middle([1,2]), []);
+   });
+});
+
+
+
+
+console.log(middle([1,2])); // expected => []
 console.log(middle([1,2,3])); // expected => [2]
 console.log(middle([1,2,3,4,5,6])); // expected => [3]
