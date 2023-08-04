2a9acf7a85a5b1c1746671ca36c40ac39f9415ce
Shilpa <shilpa.pulipati24@gmail.com>
2023-08-03 23:46:37 -0400

    refactoring head.js file

--- head.js
+++ head.js
@@ -4,7 +4,5 @@ const head = function(arr1) {
   return arr1[0];
 };
 
+module.exports = head;
 
-console.log(assertEqual(head([5,6,7]), 5));
-
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
