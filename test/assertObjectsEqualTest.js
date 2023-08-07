7f539ada14f869c52676f682430c3d32e0e85570
Shilpa <shilpa.pulipati24@gmail.com>
2023-08-07 19:31:13 -0400

    moving test code to test file

---
+++ test/assertObjectsEqualTest.js
@@ -0,0 +1,8 @@
+const assertObjectsEqual = require('../assertObjectsEqual');
+
+assertObjectsEqual({ a: '1', b: 2 },{ b: 2, a: '1' });  // passed
+assertObjectsEqual({ a: '1', b: 2 },{ b: 2, a: '1', c: 3 });  //failed
+assertObjectsEqual("lighthouse", "labs");  //failed
+assertObjectsEqual("lighthouse", "lighthouse");  //passed
+
+module.exports = assertObjectsEqual;
