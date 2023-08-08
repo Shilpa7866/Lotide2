986f222599c60ea13fd1a89a039f9a51c7ad8bb6
Shilpa <shilpa.pulipati24@gmail.com>
2023-08-07 20:01:45 -0400

    refactoring findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,34 +1,11 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(" ✅✅✅ Assertion Passed: [+actual+] === [+expected]+");
-  }
-  else if (actual !== expected) {
-    console.log(" 🛑🛑🛑 Assertion Failed: [+actual+] !== [+expected+]");
-  }
-
-};
-
 const findKeyByValue = function (object, value) {
-  let loop = Object.values(object)
-  let key = Object.keys(object)
-  let output = ""
-  for (let i = 0; i < loop.length; i++) {
-
-    if (loop[i] === value) {
-      output = key[i]
-
+  for (let key in object) {
+    if (object[key] === value) {
+      return key;
     }
   }
-  return output
-}
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire"
+  return undefined;
 };
 
-console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
-console.log(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"));
-console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+module.exports = findKeyByValue;
