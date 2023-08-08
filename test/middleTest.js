32362725f42cfae038229c0324171ef22fd74b23
Shilpa <shilpa.pulipati24@gmail.com>
2023-08-07 20:35:43 -0400

    add test files

--- test/middleTest.js
+++ test/middleTest.js
@@ -17,7 +17,3 @@ const middle = require ('../middle');
 
 
 
-
-console.log(middle([1,2])); // expected => []
-console.log(middle([1,2,3])); // expected => [2]
-console.log(middle([1,2,3,4,5,6])); // expected => [3]
